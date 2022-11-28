9cdfec33fa4a61da0eb20a4294fd24814a13c291
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2022-11-29 01:18:52 +0300

    Initial Commit

---
+++ QuickForum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickForum
+//
+//  Created by İbrahim Bayram on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
